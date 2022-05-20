bf8dca14db456b0c8e786fc4caf5ec3f772c1729
bensteUEM <Benedict.Stein@gmail.com>
2022-05-20 12:31:34 +0200

    Moved to separate Class and started to create test cases. More cleanup and remaining test cases and fixes related to proofing them and rename file of Test

--- ChurchToolsApi.py
+++ ChurchToolsApi.py
@@ -7,7 +7,11 @@ import requests
 
 class ChurchToolsApi:
     def __init__(self, domain):
+        self.session = None
         self.domain = domain
+
+        from secure.secrets import ct_token
+        self.login_ct_rest_api(ct_token)
 
     def login_ct_ajax_api(self, user="beamer_maki@evang-kirche-baiersbronn.de", pswd=""):
         """
@@ -19,45 +23,46 @@ class ChurchToolsApi:
         self.session = requests.Session()
         login_url = self.domain + '/?q=login/ajax&func=login'
         data = {'email': user, 'password': pswd}
-        self.session.post(url=login_url, data=data)
-        #TODO return success
+
+        response = self.session.post(url=login_url, data=data)
+        if response.status_code == 200:
+            logging.info('Ajax User Login Successful')
+            return json.loads(response.content)["status"] == 'success'
+        else:
+            logging.warning("Token Login failed with {}".format(response.content.decode()))
+            return False
 
     def login_ct_rest_api(self, login_token):
         """
         Authorization: Login<token>
-        If you want to authorized a request, you need to provide a Login Token as
+        If you want to authorize a request, you need to provide a Login Token as
         Authorization header in the format {Authorization: Login<token>}
         Login Tokens are generated in "Berechtigungen" of User Settings
         :param login_token: token to be used for login into CT
         :return: if login successful
         """
 
-        session = requests.Session()
+        self.session = requests.Session()
         login_url = self.domain + '/api/whoami'
         headers = {"Authorization": 'Login ' + login_token}
-        response = session.get(url=login_url, headers=headers)
+        response = self.session.get(url=login_url, headers=headers)
 
         if response.status_code == 200:
             response_content = json.loads(response.content)
-            logging.info('Login Successful as {}'.format(response_content['data']['email']))
-            self.session = session
+            logging.info('Token Login Successful as {}'.format(response_content['data']['email']))
+            return json.loads(response.content)['data']['id'] == 9
         else:
-            logging.warning("Login failed with {}".format(response.content.decode()))
-
-        #TODO return success
+            logging.warning("Token Login failed with {}".format(response.content.decode()))
+            return False
 
     def get_ct_csrf_token(self):
         """
         Requests CSRF Token https://hilfe.church.tools/wiki/0/API-CSRF
         This method was created when debugging file upload but was no longer required later on
-        :param session:
         :return: str token
         """
         url = self.domain + '/api/csrftoken'
-        headers = {
-            'accept': 'application/json',
-        }
-        response = self.session.get(url=url, headers=headers)
+        response = self.session.get(url=url)  # , headers=headers)
         if response.status_code == 200:
             csrf_token = json.loads(response.content)["data"]
             logging.info("CSRF Token erfolgreich abgerufen {}".format(csrf_token))
@@ -76,7 +81,11 @@ class ChurchToolsApi:
         }
         response = self.session.post(url=url, headers=headers)
         if response.status_code == 200:
-            print("Response Test Connection erfolgreich")
+            logging.debug("Response AJAX Connection successful")
+            return True
+        else:
+            logging.debug("Response AJAX Connection failed with {}".format(json.load(response.content)))
+            return False
 
     def get_songs(self, song_id=None):
         """ Gets list of all songs from the server
@@ -90,10 +99,28 @@ class ChurchToolsApi:
             'accept': 'application/json'
         }
         response = self.session.get(url=url, headers=headers)
+
         if response.status_code == 200:
-            response_content = json.loads(response.content)['data']
-            logging.debug("Response successful {}".format(response_content))
-            return response_content
+            response_content = json.loads(response.content)
+            response_data = response_content['data'].copy()
+            logging.debug("First response of GET Songs successful {}".format(response_content))
+
+            if 'meta' not in response_content.keys():  # Shortcut without Pagination
+                return response_data
+
+            # Long part extending results with pagination
+            while response_content['meta']['pagination']['current'] \
+                    < response_content['meta']['pagination']['lastPage']:
+                logging.info("page {} of {}".format(response_content['meta']['pagination']['current'],
+                                                    response_content['meta']['pagination']['lastPage']))
+                params = {'page': response_content['meta']['pagination']['current'] + 1}
+                response = self.session.get(url=url, headers=headers, params=params)
+                response_content = json.loads(response.content)
+                response_data.extend(response_content['data'])
+
+            return response_data
+        else:
+            logging.warning("Something went wrong fetiching songs: {}".format(response.status_code))
 
     def get_groups(self, group_id=None):
         """ Gets list of all groups
@@ -107,40 +134,62 @@ class ChurchToolsApi:
             'accept': 'application/json'
         }
         response = self.session.get(url=url, headers=headers)
-        if response.status_code == 200:
-            response_content = json.loads(response.content)['data']
-            logging.debug("Response successful {}".format(response_content))
-            return response_content
 
-    def file_upload(self, filenamepath_for_upload, domain_type='song_arrangement', domain_identifier=394,
-                    file_name=None,
+        if response.status_code == 200:
+            response_content = json.loads(response.content)
+            response_data = response_content['data'].copy()
+            logging.debug("First response of Groups successful {}".format(response_content))
+
+            if 'meta' not in response_content.keys():  # Shortcut without Pagination
+                return response_data
+
+            # Long part extending results with pagination
+            while response_content['meta']['pagination']['current'] \
+                    < response_content['meta']['pagination']['lastPage']:
+                logging.info("page {} of {}".format(response_content['meta']['pagination']['current'],
+                                                    response_content['meta']['pagination']['lastPage']))
+                params = {'page': response_content['meta']['pagination']['current'] + 1}
+                response = self.session.get(url=url, headers=headers, params=params)
+                response_content = json.loads(response.content)
+                response_data.extend(response_content['data'])
+
+            return response_data
+        else:
+            logging.warning("Something went wrong fetiching songs: {}".format(response.status_code))
+
+    def file_upload(self, filename_path_for_upload, domain_type, domain_identifier,
+                    custom_file_name=None,
                     overwrite=False):
         """
         Helper function to upload an attachment to any module of ChurchTools
-
-        :param filenamepath_for_upload: file to be opened e.g. with open('media/pinguin.png', 'rb')
-        :param domain_type:  The domain type. Currently supported are 'avatar', 'groupimage', 'logo', 'attatchments',
+        :param filename_path_for_upload: file to be opened e.g. with open('media/pinguin.png', 'rb')
+        :param domain_type:  The domain type, currently supported are 'avatar', 'groupimage', 'logo', 'attatchments',
          'html_template', 'service', 'song_arrangement', 'importtable', 'person', 'familyavatar', 'wiki_.?'.
         :param domain_identifier: ID of the object in ChurchTools
-        :param file_name: optional file name - if not specified the one from the file is used
-        :param overwrite: if true delete existing file before upload of new one to replace it's content instead of creating a copy
+        :param custom_file_name: optional file name - if not specified the one from the file is used
+        :param overwrite: if true delete existing file before upload of new one to replace \
+        it's content instead of creating a copy
         :return:
         """
 
-        file_to_upload = open(filenamepath_for_upload, 'rb')
+        file_to_upload = open(filename_path_for_upload, 'rb')
 
         url = '{}/api/files/{}/{}'.format(self.domain, domain_type, domain_identifier)
 
         if overwrite:
             logging.debug("deleting old file before download")
-            delete_file_name = file_to_upload.name if file_name is None else file_name
+            delete_file_name = file_to_upload.name if custom_file_name is None else custom_file_name
             self.file_delete(domain_type, domain_identifier, delete_file_name)
 
         # add files as files form data with dict using 'files[]' as key and (tuple of filename and fileobject)
-        if file_name is None:
+        if custom_file_name is None:
             files = {'files[]': (file_to_upload.name.split('/')[-1], file_to_upload)}
         else:
-            files = {'files[]': (file_name, file_to_upload)}
+            if '/' in custom_file_name:
+                logging.warning('/ in file name ({}) will fail upload!'.format(custom_file_name))
+                files = {}
+            else:
+                files = {'files[]': (custom_file_name, file_to_upload)}
 
         response = self.session.post(url=url, files=files)
         file_to_upload.close()
@@ -164,21 +213,19 @@ class ChurchToolsApi:
         else:
             logging.warning(response.content.decode())
 
-    def file_delete(self, domain_type='song_arrangement', domain_identifier=394, filename_for_delete=None):
+    def file_delete(self, domain_type, domain_identifier, filename_for_selective_delete=None):
         """
         Helper function to delete ALL attachments of any specified module of ChurchTools#
         Downloads all existing files to tmp and reuploads them in case filename_for_delete is specified
-
-        :param domain_type:  The domain type. Currently supported are 'avatar', 'groupimage', 'logo', 'attatchments',
+        :param domain_type:  The domain type, currently supported are 'avatar', 'groupimage', 'logo', 'attatchments',
          'html_template', 'service', 'song_arrangement', 'importtable', 'person', 'familyavatar', 'wiki_.?'.
-         defaults to song_arrangement for testing
-        :param domain_identifier: ID of the object in ChurchTools defaults to test song
-        :param filename_for_delete: name of the file to be deleted - all others will reupload
-        :return:
+        :param domain_identifier: ID of the object in ChurchTools
+        :param filename_for_selective_delete: name of the file to be deleted - all others will reupload
+        :return: if successful
         """
         url = self.domain + '/api/files/{}/{}'.format(domain_type, domain_identifier)
 
-        if filename_for_delete is not None:
+        if filename_for_selective_delete is not None:
 
             response = self.session.get(url=url)
             online_files = [(item["id"], item['name'], item['fileUrl']) for item in
@@ -188,8 +235,8 @@ class ChurchToolsApi:
                 current_id = current_file[0]
                 current_name = current_file[1]
                 current_url = current_file[2]
-                if current_name != filename_for_delete:
-                    response = self.session.get(current_file[2])
+                if current_name != filename_for_selective_delete:
+                    response = self.session.get(current_url)
                     file_path_name = "tmp/{}_{}".format(current_id, current_name)
                     temp_file = open(file_path_name, "wb")
                     temp_file.write(response.content)
@@ -199,9 +246,12 @@ class ChurchToolsApi:
         response = self.session.delete(url=url)
 
         # Recover Local Files with new upload and delete local copy from tmp
-        if filename_for_delete is not None:
+        if filename_for_selective_delete is not None:
             local_files = os.listdir('tmp')
             for current_file in local_files:
                 file_name = ''.join((current_file.split("_")[1:]))
-                self.file_upload('tmp/{}'.format(current_file), file_name=file_name)
+                self.file_upload('tmp/{}'.format(current_file), domain_type, domain_identifier,
+                                 custom_file_name=file_name)
                 os.remove('tmp/{}'.format(current_file))
+
+        return response.status_code == 204  # success code for delete action upload
--- main.py
+++ main.py
@@ -15,21 +15,6 @@ if __name__ == '__main__':
     api.login_ct_rest_api(ct_token)
     api.check_connection_ajax()
 
-    # Tries to upload a file to the test song with ID 394
-    # song = get_songs(current_session, 394)
-
-    # Tries to upload a 2 files to the test song with ID 394
-    api.file_upload('media/pinguin.png', "song_arrangement", 394)
-    api.file_upload('media/pinguin_shell.png', "song_arrangement", 394, 'pinguin_shell.png')
-
-    # Adds the same file again without overwrite - should exist twice
-    api.file_upload('media/pinguin.png', "song_arrangement", 394, 'pinguin.png')
-    # Adds the same file again without overwrite - should only exist onces and with shell style but normal name
-    api.file_upload('media/pinguin_shell.png', "song_arrangement", 394, 'pinguin.png', overwrite=True)
-
-    # Delte all Files because of testing
-    api.file_delete("song_arrangement", 394, filename_for_delete='pinguin.png')
-
     # Tries to upload a file as group image for test group 103
     group = api.get_groups(103)
     api.file_upload('media/pinguin.png', "groupimage", 103)
