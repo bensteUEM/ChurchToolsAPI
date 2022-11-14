c01f67daee237f02bf032863b0c0666ef3c98832
bensteUEM <Benedict.Stein@gmail.com>
2022-11-14 17:00:23 +0100

    saved some functionality recently used to mod CT entries

--- main.py
+++ main.py
@@ -2,21 +2,30 @@ import logging
 
 from ChurchToolsApi import ChurchToolsApi
 
+
+def assign_specific_tag_to_all_songs(api):
+    """
+    Helper to append a tag to all songs - e.g. 51 is tag:"in ChurchTools vor Skript Import"
+    :param api:
+    :return:
+    """
+    songs = api.get_songs()
+    all_song_ids = [value['id'] for value in songs]
+    for id in all_song_ids:
+        api.add_song_tag(id, 51)
+
+
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
     logging.info("Started main")
 
-    from secure.defaults import domain as domain_temp
 
     # Create Session
+    from secure.defaults import domain as domain_temp
     from secure.secrets import ct_token
-
     api = ChurchToolsApi(domain_temp)
     api.login_ct_rest_api(ct_token)
-    api.check_connection_ajax()
 
-    # Tries to upload a file as group image for test group 103
-    # group = api.get_groups(103)
-    # api.file_upload('media/pinguin.png', "groupimage", 103)
+    #assign_specific_tag_to_all_songs(api)
 
     logging.info("finished main")
