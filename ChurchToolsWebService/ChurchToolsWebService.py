8a0843ec7aef44ba6a600c6f1403a2685574bc88
bensteUEM <Benedict.Stein@gmail.com>
2023-02-23 21:52:27 +0100

    #41 introducing session for storing API object on successful login

--- ChurchToolsWebService/ChurchToolsWebService.py
+++ ChurchToolsWebService/ChurchToolsWebService.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template, request, redirect
+import os
+
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
 
 from ChurchToolsAPI.ChurchToolsApi import ChurchToolsApi as CTAPI
 from secure.defaults import domain
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 @app.route('/')
 def index():
-    return redirect('/login')
+    if 'ct_api' in session:
+        return redirect('/main')
+    else:
+        return redirect('/login')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -21,8 +32,8 @@ def login():
         user = request.form['user']
         password = request.form['password']
 
-        app.ct_api = CTAPI(domain, ct_user=user, ct_password=password)
-        if app.ct_api.who_am_i() is not False:
+        session['ct_api'] = CTAPI(domain, ct_user=user, ct_password=password)
+        if session['ct_api'].who_am_i() is not False:
             return redirect('/main')
 
         error = 'Invalid Login'
@@ -33,9 +44,9 @@ def login():
 
 @app.route('/main')
 def main():
-    user = app.ct_api.who_am_i()
+    user = session['ct_api'].who_am_i()
     return render_template('main.html', user=user, domain=domain)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
