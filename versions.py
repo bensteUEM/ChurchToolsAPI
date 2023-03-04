b4be81353d8981a578304e97f43acd862ef766e9
bensteUEM <Benedict.Stein@gmail.com>
2023-03-04 13:07:44 +0100

    version number file changes for github actions variable

--- versions.py
+++ versions.py
@@ -1,7 +1,7 @@
 import os
 
-VERSION = '1.2.3.7'
+VERSION = '1.2.3.8'
 
 if __name__ == '__main__':
     os.environ['VERSION'] = VERSION
-    print(f"::set-output name=VERSION::{os.environ['VERSION']}")
+    print(f"::set-output name=VERSION::{VERSION}")
