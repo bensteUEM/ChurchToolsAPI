56030caadfac4a7776ea47e0ebe600f08bab5b63
bensteUEM <Benedict.Stein@gmail.com>
2024-09-22 17:37:19 +0200

    implemented /bookings - fixes #97

--- churchtools_api/resources.py
+++ churchtools_api/resources.py
@@ -46,3 +46,78 @@ class ChurchToolsApiResources(ChurchToolsApiAbstract):
         else:
             logging.error(response)
             return
+
+    def get_bookings(self, **kwargs) -> list[dict]:
+        """_summary_
+
+        Arguments:
+            kwargs - see list below - some combination limits do apply
+
+        KwArgs:
+            booking_id: only one booking by id (use standalone only)
+            resources_ids:list[int] - required if not booking_id
+            status_ids: filter by list of stats ids to consider (requires resource_ids)
+            _from: date range to consider (use only with _to! - might have a bug in API - Support Ticket 130123)
+            _to: date range to consider (use only with _from! - might have a bug in API - Support Ticket 130123)
+            appointment_id: get resources for one specific calendar_appointment only (use together with _to and _from for performance reasons)
+        """
+        url = self.domain + "/api/bookings"
+        headers = {"accept": "application/json"}
+        params = {}
+
+        # at least one of the following arguments is required
+        required_kwargs = ["booking_id", "resource_ids"]
+        if not any([kwarg in kwargs for kwarg in required_kwargs]):
+            logging.error(
+                "invalid argument combination in get_bookings - please check docstring for requirements"
+            )
+            return
+
+        if booking_id := kwargs.get("booking_id"):
+            url = url + "/{}".format(kwargs["booking_id"])
+        elif resource_ids := kwargs.get("resource_ids"):
+            params["resource_ids[]"] = resource_ids
+
+            if status_ids := kwargs.get("status_ids"):
+                params["status_ids[]"] = status_ids
+            if "_from" in kwargs or "_to" in kwargs:
+                if ("_from" in kwargs and "_to" not in kwargs) or (
+                    "_from" not in kwargs and "_to" in kwargs
+                ):
+                    logging.warning(
+                        "See ChurchTools support ticket 130123 - might have unexpected behaviour if to and from are used standalone"
+                    )
+                if _from := kwargs.get("_from"):
+                    params["from"] = _from.strftime("%Y-%m-%d")
+                if _to := kwargs.get("_to"):
+                    params["to"] = _to.strftime("%Y-%m-%d")
+            if appointment_id := kwargs.get("appointment_id"):
+                if "from" not in params:
+                    logging.info(
+                        "Performance and stability issues - use appointment_id param together with to and from for faster results and definite time range"
+                    )
+                params["appointment_id"] = appointment_id
+
+        response = self.session.get(url=url, headers=headers, params=params)
+
+        if response.status_code == 200:
+            response_content = json.loads(response.content)
+
+            response_data = self.combine_paginated_response_data(
+                response_content, url=url, headers=headers
+            )
+            result_list = (
+                [response_data] if isinstance(response_data, dict) else response_data
+            )
+
+            if appointment_id := kwargs.get("appointment_id"):
+                return [
+                    i
+                    for i in result_list
+                    if i["base"]["appointmentId"] == appointment_id
+                ]
+            else:
+                return result_list
+        else:
+            logging.error(response.content)
+            return
--- tests/test_churchtools_api_resources.py
+++ tests/test_churchtools_api_resources.py
@@ -1,5 +1,5 @@
+from datetime import datetime, timedelta
 import logging
-import pytest
 from tests.test_churchtools_api_abstract import TestsChurchToolsApiAbstract
 
 
@@ -48,3 +48,176 @@ class Test_churchtools_api_resources(TestsChurchToolsApiAbstract):
         self.api.get_resource_masterdata(result_type="")
         expected_error_message = "get_resource_masterdata does not know result_type="
         assert expected_error_message in caplog.messages
+
+    def test_get_booking_by_id(self):
+        """IMPORTANT - This test method and the parameters used depend on the target system!
+        the hard coded sample exists on ELKW1610.KRZ.TOOLS"""
+
+        SAMPLE_BOOKING_ID = 5108
+        result = self.api.get_bookings(booking_id=SAMPLE_BOOKING_ID)
+        assert SAMPLE_BOOKING_ID == result[0]["id"]
+
+    def test_get_booking_by_resource_ids(self):
+        """IMPORTANT - This test method and the parameters used depend on the target system!
+        the hard coded sample exists on ELKW1610.KRZ.TOOLS"""
+        RESOURCE_ID_SAMPLES = [8, 20]
+        result = self.api.get_bookings(resource_ids=RESOURCE_ID_SAMPLES)
+        result_resource_ids = {i["base"]["resource"]["id"] for i in result}
+        assert set(RESOURCE_ID_SAMPLES) == result_resource_ids
+
+    def test_get_booking_by_status_ids(self, caplog):
+        """IMPORTANT - This test method and the parameters used depend on the target system!
+        the hard coded sample exists on ELKW1610.KRZ.TOOLS"""
+        caplog.set_level(logging.ERROR)
+        STATUS_ID_SAMPLES = [2]
+        self.api.get_bookings(status_ids=STATUS_ID_SAMPLES)
+        expected_response = "invalid argument combination in get_bookings - please check docstring for requirements"
+        assert expected_response in caplog.messages
+
+    def test_get_booking_by_resource_and_status_ids(self):
+        """IMPORTANT - This test method and the parameters used depend on the target system!
+        the hard coded sample exists on ELKW1610.KRZ.TOOLS"""
+        STATUS_ID_SAMPLES = [2]
+        RESOURCE_ID_SAMPLES = [8]
+        result = self.api.get_bookings(
+            resource_ids=RESOURCE_ID_SAMPLES, status_ids=STATUS_ID_SAMPLES
+        )
+        assert set(RESOURCE_ID_SAMPLES) == {i["base"]["resource"]["id"] for i in result}
+        assert set(STATUS_ID_SAMPLES) == {i["base"]["statusId"] for i in result}
+
+    def test_get_booking_from_to_date_without_resource_id(self, caplog):
+        """IMPORTANT - This test method and the parameters used depend on the target system!
+        the hard coded sample exists on ELKW1610.KRZ.TOOLS"""
+        caplog.set_level(logging.WARNING)
+        SAMPLE_DATES = {
+            "_from": datetime(year=2024, month=12, day=24),
+            "_to": datetime(year=2024, month=12, day=24),
+        }
+        self.api.get_bookings(_from=SAMPLE_DATES["_from"])
+
+        expected_response = "invalid argument combination in get_bookings - please check docstring for requirements"
+        assert expected_response in caplog.messages
+
+    def test_get_booking_from_date(self, caplog):
+        """IMPORTANT - This test method and the parameters used depend on the target system!
+        the hard coded sample exists on ELKW1610.KRZ.TOOLS"""
+        RESOURCE_ID_SAMPLES = [8, 20]
+        SAMPLE_DATES = {
+            "_from": datetime(year=2024, month=9, day=21),
+            "_to": datetime(year=2024, month=9, day=30),
+        }
+
+        result = self.api.get_bookings(
+            _from=SAMPLE_DATES["_from"], resource_ids=RESOURCE_ID_SAMPLES
+        )
+        assert set(RESOURCE_ID_SAMPLES) == {i["base"]["resource"]["id"] for i in result}
+
+        result_dates = {
+            datetime.strptime(i["calculated"]["startDate"][:10], "%Y-%m-%d")
+            for i in result
+        }
+        assert all(
+            [SAMPLE_DATES["_from"] <= compare_date for compare_date in result_dates]
+        )
+
+        expected_response = "See ChurchTools support ticket 130123 - might have unexpected behaviour if to and from are used standalone"
+        assert expected_response in caplog.messages
+
+    def test_get_booking_to_date(self, caplog):
+        """IMPORTANT - This test method and the parameters used depend on the target system!
+        the hard coded sample exists on ELKW1610.KRZ.TOOLS"""
+        RESOURCE_ID_SAMPLES = [8, 20]
+        SAMPLE_DATES = {
+            "_from": datetime(year=2024, month=9, day=21),
+            "_to": datetime(year=2024, month=9, day=30),
+        }
+
+        result = self.api.get_bookings(
+            _to=SAMPLE_DATES["_to"], resource_ids=RESOURCE_ID_SAMPLES
+        )
+        assert set(RESOURCE_ID_SAMPLES) == {i["base"]["resource"]["id"] for i in result}
+
+        result_dates = {
+            datetime.strptime(i["calculated"]["startDate"][:10], "%Y-%m-%d")
+            for i in result
+        }
+        assert all(
+            [SAMPLE_DATES["_to"] >= compare_date for compare_date in result_dates]
+        )
+
+        expected_response = "See ChurchTools support ticket 130123 - might have unexpected behaviour if to and from are used standalone"
+        assert expected_response in caplog.messages
+
+    def test_get_booking_from_to_date(self, caplog):
+        """IMPORTANT - This test method and the parameters used depend on the target system!
+        the hard coded sample exists on ELKW1610.KRZ.TOOLS"""
+        RESOURCE_ID_SAMPLES = [8, 20]
+        SAMPLE_DATES = {
+            "_from": datetime(year=2024, month=9, day=21),
+            "_to": datetime(year=2024, month=9, day=30),
+        }
+
+        result = self.api.get_bookings(
+            _from=SAMPLE_DATES["_from"],
+            _to=SAMPLE_DATES["_to"],
+            resource_ids=RESOURCE_ID_SAMPLES,
+        )
+        assert set(RESOURCE_ID_SAMPLES) == {i["base"]["resource"]["id"] for i in result}
+
+        result_dates = {
+            datetime.strptime(i["calculated"]["startDate"][:10], "%Y-%m-%d")
+            for i in result
+        }
+        assert all(
+            [SAMPLE_DATES["_from"] <= compare_date for compare_date in result_dates]
+        )
+        assert all(
+            [SAMPLE_DATES["_to"] >= compare_date for compare_date in result_dates]
+        )
+
+        assert [] == caplog.messages
+
+    def test_get_booking_appointment_id(self, caplog):
+        """IMPORTANT - This test method and the parameters used depend on the target system!
+        the hard coded sample exists on ELKW1610.KRZ.TOOLS"""
+        caplog.set_level(logging.INFO)
+
+        RESOURCE_ID_SAMPLES = [16]
+        SAMPLE_APPOINTMENT_ID = 327883  # 22.9 GH
+        result = self.api.get_bookings(
+            appointment_id=SAMPLE_APPOINTMENT_ID, resource_ids=RESOURCE_ID_SAMPLES
+        )
+        expected_log_message = "Performance and stability issues - use appointment_id param together with to and from for faster results and definite time range"
+        assert expected_log_message in caplog.messages
+        assert len(result) == 1
+        assert result[0]["base"]["caption"] == "Zentral-Gottesdienst im Gemeindehaus"
+        assert result[0]["base"]["resource"]["id"] in set(RESOURCE_ID_SAMPLES)
+
+    def test_get_booking_appointment_id_daterange(self):
+        """IMPORTANT - This test method and the parameters used depend on the target system!
+        the hard coded sample exists on ELKW1610.KRZ.TOOLS"""
+        RESOURCE_ID_SAMPLES = [16]
+
+        SAMPLE_APPOINTMENT_ID = 327883
+        SAMPLE_DATES = {
+            "_from": datetime(year=2024, month=9, day=22),
+            "_to": datetime(year=2024, month=9, day=22),
+        }
+
+        result = self.api.get_bookings(
+            _from=SAMPLE_DATES["_from"],
+            _to=SAMPLE_DATES["_to"],
+            appointment_id=SAMPLE_APPOINTMENT_ID,
+            resource_ids=RESOURCE_ID_SAMPLES,
+        )
+
+        result_date = datetime.strptime(
+            result[0]["calculated"]["startDate"][:10], "%Y-%m-%d"
+        )
+
+        assert len(result) == 1
+        # check dates incl. max 1 day diff because of reservations before event start
+        assert SAMPLE_DATES["_from"] - timedelta(days=1) <= result_date
+        assert SAMPLE_DATES["_to"] + timedelta(days=1) >= result_date
+        assert result[0]["base"]["caption"] == "Zentral-Gottesdienst im Gemeindehaus"
+        assert result[0]["base"]["resource"]["id"] in set(RESOURCE_ID_SAMPLES)
