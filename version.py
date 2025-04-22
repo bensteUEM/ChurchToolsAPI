8fb47b87b12025800570f83b66c309deb08935d9
bensteUEM <Benedict.Stein@gmail.com>
2025-04-22 21:46:54 +0200

    feat: implemented release automation (#154)

--- version.py
+++
@@ -1,9 +0,0 @@
-"""helper script to define version number for automation."""
-
-import os
-
-VERSION = "1.7.3"
-__version__ = VERSION
-
-if __name__ == "__main__":
-    os.environ["VERSION"] = VERSION
